10b4ee6ff2657cb17739d69689aee0a495d6c195
MAYUKH GHOSH <93669449+Mack69@users.noreply.github.com>
2023-03-23 18:53:45 +0530

    Login Form with Comments

--- FormLogin.vb
+++ FormLogin.vb
@@ -1,51 +1,81 @@
-﻿
 Imports System.Data.SqlClient
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement
 
 Public Class FormLogin
 
-    Dim email As String
+    Dim email, password As String
     Private Sub FormLogin_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'setting error alert images to false initialluy
         PictureBoxErrorLogPassword.Visible = False
-        PictureBoxErrorLogUsername.Visible = False
+        PictureBoxErrorLogEmail.Visible = False
     End Sub
 
     Private Sub ButtonLogin_Click(sender As Object, e As EventArgs) Handles ButtonLogin.Click
+        'storing email and password to variables
         email = TextBoxLogEmail.Text
+        password = TextBoxLogPassword.Text
+
         userlogin()
     End Sub
 
-    Private Sub ButtonRegister_Click(sender As Object, e As EventArgs)
+    Private Sub LabelCreateAccount_Click(sender As Object, e As EventArgs) Handles LabelCreateAccount.Click
+        'opening registeration page
         Me.Hide()
         FormRegister.Show()
     End Sub
 
     Sub userlogin()
+        'setting the error alert images to false
         PictureBoxErrorLogPassword.Visible = False
-        PictureBoxErrorLogUsername.Visible = False
+        PictureBoxErrorLogEmail.Visible = False
+
+        'for addmin Login
+        If (email = "admin@gmail.com" And password = "Admin123") Then
+            logedInEmail = email
+            Me.Hide()
+            FormAdminCards.Show()
+            Return
+        End If
+
+        'setting sql connection
         Dim conn As New SqlConnection
         Dim cmd As New SqlCommand
-        conn.ConnectionString = "Data Source=DESKTOP-A26E0MD;Initial Catalog=SE_PROJECT;Integrated Security=True; "
+        conn.ConnectionString = "Data Source=LAPTOP-G773S8H7;Initial Catalog=SE-PROJECT;Integrated Security=True;"
         conn.Open()
         Try
-            Dim check As String = "SELECT * FROM table_user WHERE EMAIL = '" & email & "' AND PASSWORD = '" & TextBoxLogPassword.Text & " ' "
-            cmd = New SqlCommand(check, conn)
+            'selecting the password for the entered email
+            Dim checkEmail = "SELECT Password FROM TableUser WHERE Email = '" & email & "'"
+            Dim sqlPassword As String
+            cmd = New SqlCommand(checkEmail, conn)
             Dim reader As SqlDataReader = cmd.ExecuteReader
 
+            'if the email existes it will return a password
+            'if the passord esxists
             If reader.Read Then
-                MessageBox.Show("LOGIN SUCCESSFUL !!")
-                Me.Hide()
-                FormChooseType.Show()
+                sqlPassword = reader("Password")
+                'checking if the password is correct
+                If (sqlPassword = password) Then
+                    logedInEmail = email
+                    Me.Hide()
+                    FormCardList.Show()
+
+                    'if the password is incorrect
+                Else
+                    PictureBoxErrorLogPassword.Visible = True
+                End If
+
+                'if the email does not exists
             Else
-                MessageBox.Show("INVALID LOGIN CREDENTIALS !!")
-                PictureBoxErrorLogPassword.Visible = True
-                PictureBoxErrorLogUsername.Visible = True
+                MessageBox.Show("Your Email does not exist please REGISTER")
+                PictureBoxErrorLogEmail.Visible = True
             End If
 
         Catch ex As Exception
             MsgBox("MSSQL ERROR")
         End Try
-
-        conn.close()
-    
+        conn.Close()
     End Sub
+
+
 End Class
+
